6a18d4fb6d2a42e96b17be78455f2f01e11b948d
MuratYaman <muratymn.72@gmail.com>
2017-08-01 09:24:26 +0300

    TodoList React-native

---
+++ app/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+
+import NotDefterim from './scenes/NoteDefterim'
+
+export default class App extends Component {
+  render() {
+    return (
+        <NotDefterim/>
+    );
+  }
+}
